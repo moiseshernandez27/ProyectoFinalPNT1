c44b9e1d2448c49f1ef7d5967754da5fb6599d12
Braian Molina <braian_gabriel_1993@hotmail.com>
2024-07-06 22:12:41 -0300

    se integra base del jwt

--- Program.cs
+++ Program.cs
@@ -1,7 +1,38 @@
 using Microsoft.EntityFrameworkCore;
 using ProyectoFinalToDo.Data;
+using ProyectoFinalToDo.Models;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
+string accessToken = "ghuDX7VAwQ9E0l1jnC6ms5kaqUiBvrMx"; // [WEP 256-bit Key] [https://keygen.io/]
+
+// Add jwt authentication
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication("Bearer").AddJwtBearer(option =>
+    {
+        var singingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(accessToken));
+        var singingCredentials = new SigningCredentials(singingKey, SecurityAlgorithms.HmacSha256Signature);
+
+        option.RequireHttpsMetadata = false; // only for development
+
+        option.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            IssuerSigningKey = singingKey
+        };
+    }
+);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -23,10 +54,40 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+//test jwt
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/protected", () => "Hello World!").RequireAuthorization();
+app.MapGet("/protectedData", (ClaimsPrincipal user) => user.Identity?.Name).RequireAuthorization();
+
+app.MapGet("/auth/login/{user}/{pass}", (string user, string pass) =>
+{
+    if(user == "admin" && pass == "admin123")
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var byteKey = Encoding.UTF8.GetBytes(accessToken);
+        var tokenDes = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, user)
+            }),
+            Expires = DateTime.UtcNow.AddMonths(1), // token expires in 1 month
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(byteKey), SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDes);
+        return tokenHandler.WriteToken(token);
+    }
+    else
+    {
+        return "Invalid user or password";
+    }
+});
+//test
 app.Run();
